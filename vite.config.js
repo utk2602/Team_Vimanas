3e34015460fb804255003777717a520b86866c46
utkarsh <166751207+utk2602@users.noreply.github.com>
2025-01-18 13:18:31 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['framer-motion'], // Add this line
-    },
-  },
+  resolve: { alias: { 'lucide-react': path.resolve(__dirname, 'node_modules/lucide-react') } },
   resolve: { alias: { 'lucide-react': path.resolve(__dirname, 'node_modules/lucide-react') } },
 });
