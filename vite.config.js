69d1b3956deac3ef53ad0f221e909c6c91aa3b83
utkarsh <166751207+utk2602@users.noreply.github.com>
2025-01-18 13:06:28 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { rollupOptions: { external: ['framer-motion'] } }
-})
+  build: {
+    rollupOptions: {
+      external: ['framer-motion'], // Add this line
+    },
+  },
+});
